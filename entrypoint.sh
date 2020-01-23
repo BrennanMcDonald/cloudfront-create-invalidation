22966e82fe8e3083ff6f56d1f27054c01cbba9cc
Brennan McDonald <Brennan.mcdonald.mail@gmail.com>
2020-01-23 12:44:51 -0500

    Updated methods.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ if [ -z "$AWS_ITEMS_TO_INVALIDATE" ]; then
   exit 1
 fi
 
-if [ -z "$AWS_DISTRIBUTION" ]; then
-  echo "AWS_DISTRIBUTION is not set. Quitting."
+if [ -z "$AWS_CLOUDFRONT_DISTRIBUTION" ]; then
+  echo "AWS_CLOUDFRONT_DISTRIBUTION is not set. Quitting."
   exit 1
 fi
 
@@ -32,10 +32,8 @@ if [ -n "$AWS_S3_ENDPOINT" ]; then
   ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
 fi
 
-# Create a dedicated profile for this action to avoid conflicts
-# with past/future actions.
-# https://github.com/jakejarvis/s3-sync-action/issues/1
-aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+
+aws configure --profile cloudfront-invalidation <<-EOF > /dev/null 2>&1
 ${AWS_ACCESS_KEY_ID}
 ${AWS_SECRET_ACCESS_KEY}
 ${AWS_REGION}
@@ -44,16 +42,15 @@ EOF
 
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
-sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
-              --profile s3-sync-action \
-              --no-progress \
-              ${ENDPOINT_APPEND} $*"
+sh -c "aws cloudfront create-invalidation \
+    --distribution-id ${AWS_DISTRIBUTION} \
+    --paths ${$AWS_ITEMS_TO_INVALIDATE}"
 
 # Clear out credentials after we're done.
 # We need to re-run `aws configure` with bogus input instead of
 # deleting ~/.aws in case there are other credentials living there.
 # https://forums.aws.amazon.com/thread.jspa?threadID=148833
-aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+aws configure --profile cloudfront-invalidation <<-EOF > /dev/null 2>&1
 null
 null
 null
